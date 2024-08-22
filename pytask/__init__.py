059d6b83079c3d5e07d7b70ce9fb85febd0fb456
Eivind Sulen <eivindsulen2002@gmail.com>
2024-08-22 11:52:45 +0200

    Added requester to fetch ics data from the provided streams
    Streams are still hard-coded. The plan is to get streams through a command line flag,
    or to get the urls piped into the program. It should also be possible to select a task data location
    so that tasks added through this program are separate from the normal taskwarrior tasks

--- pytask/__init__.py
+++ pytask/__init__.py
@@ -1,26 +1,58 @@
 from icalendar import Calendar
 from taskw_ng import TaskWarrior
-from pathlib import Path
+import re
+import requests
+
+hvl = requests.get("https://cloud.timeedit.net/hvl/web/studbergen/ri6795Q03k22u1QZQtQn39CQZQ8QY96dZ6328Z0y66809wtZ485ZF29A20ABB0EA8CBF6000A415E04F.ics")
+hvl = hvl.text
+uib = requests.get("https://mitt.uib.no/feeds/calendars/user_urR0om7uZCMG1dcgcIpFOg1hx926MEjktxJQTPwA.ics")
+uib = uib.text
+
+print("\n------------ UiB -------------\n")
+
+calendar = Calendar.from_ical(uib)
+
+for event in calendar.walk('VEVENT')[:5]:
+    # print('UID:', event.get('UID'))
+    print('DTSTART:', event.get('DTSTART'))
+    print('DTEND:', event.get('DTEND'))
+    print('DTSTAMP:', event.get('DTSTAMP'))
+    # print('CLASS:', event.get('CLASS'))
+    # print('LOCATION:', event.get('LOCATION'))
+
+    # Retrieving Location from Description using string manipulation
+    # location = event.get('DESCRIPTION')
+    # start = location.find('[')
+    # end = location.find(']', start)
+    # location = location[start:end+1][1:-1]
+    # print('LOCATION:', location)
+
+    # Retrieving Location from Description using regular expression
+    pattern = r'\[.*?\]'
+    locations = re.findall(pattern, event.get('DESCRIPTION'))
+    cleaned_locations = ""
+    i = 1
+    for location in locations:
+        cleaned_locations += location[1:-1]
+        if i == 2:
+            break
+        cleaned_locations += ", "
+        i += 1
+    print('LOCATION:', cleaned_locations)
+
+    # print('SEQUENCE:', event.get('SEQUENCE'))
+    print('SUMMARY:', event.get('SUMMARY'))
+    # print('URL:', event.get('URL'))
+    # print('DESCRIPTION:', event.get('DESCRIPTION'))
+    # print('X-ALT-DESC:', event.get('X-ALT-DESC'))
+    print("\n----------------------------\n")
 
 
-"""
-Expected output:
-    task add INF264 - Kræsjkurs i Python frivillig - Aktivt rom 2+3
-    task add DAT351 - Forelesning - B313
-
-I want the tasks to only include location, topic, and time.
-It should also have a tag, like ics, so that they can all be removed on
-each update to prevent duplicates.
-"""
+calendar = Calendar.from_ical(hvl)
 
 print("\n------------ HVL -------------\n")
 
-ics_path = Path("ics/hvl-canvas.ics")
-
-with ics_path.open() as f:
-    calendar = Calendar.from_ical(f.read())
-
-for event in calendar.walk('VEVENT'):
+for event in calendar.walk('VEVENT')[:5]:
     print("DTSTART:", event.get("DTSTART"))
     print("DTEND:", event.get("DTEND"))
     # print("UID:", event.get("UID"))
@@ -29,31 +61,6 @@ for event in calendar.walk('VEVENT'):
     print("SUMMARY:", event.get("SUMMARY"))
     print("LOCATION:", event.get("LOCATION"))
     # print("DESCRIPTION:", event.get("DESCRIPTION"))
-    break
 
-print("\n------------ UiB -------------\n")
-
-
-ics_path = Path("ics/mittuib.ics")
-
-with ics_path.open() as f:
-    calendar = Calendar.from_ical(f.read())
-
-for event in calendar.walk('VEVENT'):
-    # print('UID:', event.get('UID'))
-    print('DTSTART:', event.get('DTSTART'))
-    print('DTEND:', event.get('DTEND'))
-    print('DTSTAMP:', event.get('DTSTAMP'))
-    # print('CLASS:', event.get('CLASS'))
-    # print('LOCATION:', event.get('LOCATION'))
-    # print('SEQUENCE:', event.get('SEQUENCE'))
-    print('SUMMARY:', event.get('SUMMARY'))
-    # print('URL:', event.get('URL'))
-    # print('DESCRIPTION:', event.get('DESCRIPTION'))
-    # print('X-ALT-DESC:', event.get('X-ALT-DESC'))
-    break
-
-# TODO: 
-# Extract position from description or x-alt-desc in UiB entries.
-# This can maybe be hard-coded.
+    print("\n----------------------------\n")
 
